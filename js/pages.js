417c397b52e4f6c31bfd4cd604b1f65c8ab9633c
Faisal N <f.naqaweh@gmail.com>
2019-03-07 23:29:00 -0500

    Ready for first release

--- js/pages.js
+++ js/pages.js
@@ -14,7 +14,7 @@ const app = {
 		history.replaceState({}, "Home", "#home-page");
 		window.addEventListener("popstate", app.poppin);
 	},
-	nav: function(ev) {
+	nav: function(ev) {	//The function called when beginning navigation
 		ev.preventDefault();
 		let currentPage = ev.target.getAttribute("data-target");
 		document.querySelector(".active").classList.remove("active");
@@ -22,7 +22,7 @@ const app = {
 		history.pushState({}, currentPage, `#${currentPage}`);
 		document.getElementById(currentPage).dispatchEvent(app.show);
 	},
-	pageShown: function(ev) {
+	pageShown: function(ev) { //The function called once the page is loaded
 		let heading = ev.target.querySelector("h2");
 		heading.classList.add("initial-big");
 		setTimeout(
@@ -32,6 +32,8 @@ const app = {
 			500,
 			heading
 		);
+		window.scrollTo(0,0); //Resets scroll bar to the top upon page landing
+		document.getElementById("nav-toggle").checked = false; //Uncheck the navbar checkbox to close the menu upon page landing
 	},
 	poppin: function(ev) {
 		console.log(location.hash, "popstate event");
